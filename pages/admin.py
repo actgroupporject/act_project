815ba0ef9fd479c7763748f2fa0a50a9aa36d61b
Kimmyeonghyeon0 <Kimmyeonghyeom0>
2024-12-05 15:54:35 +0900

    PR 충돌 해결

--- pages/admin.py
+++ pages/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-
 from .models import RecruitDetail, RecruitMain
 
-
 class RecruitDetailAdmin(admin.ModelAdmin):
-    list_display = ("id", "get_work_category", "title", "get_work_title", "director")
-    list_filter = ("recruitmain__work_category",)
+    list_display = ("id", "title", "get_work_category", "get_work_title", "casting_type", "director", "get_d_day")
+    list_filter = ("recruitmain__work_category", "casting_type")
+    search_fields = ["title", "recruitmain__work_title", "requirements"]
 
     @admin.display(description="Work Category")
     def get_work_category(self, obj):
@@ -14,6 +13,5 @@ class RecruitDetailAdmin(admin.ModelAdmin):
     @admin.display(description="Work Title")
     def get_work_title(self, obj):
         return obj.recruitmain.work_title
-
 
 admin.site.register(RecruitDetail, RecruitDetailAdmin)
